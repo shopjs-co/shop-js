a7a89b4cfdb2a6519d5dce1ff3617ca4c1ef3f1d
Rami Bitar <rbitar@gmail.com>
2022-03-27 17:33:48 -0400

    Remove next/router

--- dist/components/products/Product.js
+++ dist/components/products/Product.js
@@ -41,7 +41,7 @@ var Product = function Product(_ref) {
   }, /*#__PURE__*/_react["default"].createElement(_Image["default"], {
     alt: product.title,
     layout: "fill",
-    src: (product === null || product === void 0 ? void 0 : (_product$images = product.images) === null || _product$images === void 0 ? void 0 : (_product$images$edges = _product$images.edges[0]) === null || _product$images$edges === void 0 ? void 0 : (_product$images$edges2 = _product$images$edges.node) === null || _product$images$edges2 === void 0 ? void 0 : _product$images$edges2.src) || ''
+    src: (product === null || product === void 0 ? void 0 : (_product$images = product.images) === null || _product$images === void 0 ? void 0 : (_product$images$edges = _product$images.edges[0]) === null || _product$images$edges === void 0 ? void 0 : (_product$images$edges2 = _product$images$edges.node) === null || _product$images$edges2 === void 0 ? void 0 : _product$images$edges2.src) || _constants.EMPTY_IMAGE_URL
   })), /*#__PURE__*/_react["default"].createElement(_material.Box, {
     py: 1,
     px: 1
